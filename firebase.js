357f8cb68c47002db7f240d82c039dd93e58b595
bharathuse21-bit <bharathuse21@gmail.com>
2025-11-18 22:24:28 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAivdKu90qWik88ykWxgvs4ZR2DTBRFykA",
   authDomain: "medstudyeasy-f1e25.firebaseapp.com",
   projectId: "medstudyeasy-f1e25",
-  storageBucket: "medstudyeasy-f1e25.firebasestorage.app",
+  storageBucket: "medstudyeasy-f1e25.appspot.com", // <-- fixed here
   messagingSenderId: "479468659075",
   appId: "1:479468659075:web:ac7949ee17a6832a2c56b6",
   measurementId: "G-N96DW95QRK"
